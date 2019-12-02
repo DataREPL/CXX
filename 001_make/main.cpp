f6bfb03a23ed4451ed02ce1fa7066ceca8520b24
Enin Xue <andy.xueyi@hotmail.com>
2019-12-02 15:11:27 +0800

    x

---
+++ 001_make/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author      : Xue Yi (andy_xueyi@hotmail.com)
+ * @file        : main
+ * @created     : Monday Dec 02, 2019 13:53:50 CST
+ */
+
+#include <iostream>
+#include "Animal.h"
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    Animal a(10, "Bunny");
+    a.say();
+    return 0;
+}
+
